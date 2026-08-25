2e4d3800a10bf4f013723662d30a599c8786e17b
leru-sama <leru-sama@users.noreply.github.com>
2026-08-25 22:03:36 +0000

    feat: add VCS and Verdi targets to Makefile
    - Add 'make vcs' target: compile and simulate with Synopsys VCS
    - Add 'make vcs_compile' target: compile only, no run
    - Add 'make verdi' target: launch Verdi waveform viewer
    - Update filelist.f paths to be relative to project root
    - Update clean target to remove VCS artifacts
    - Update help text with new targets

--- jesd204_pcs_standalone/script/filelist.f
+++ jesd204_pcs_standalone/script/filelist.f
@@ -1,38 +1,38 @@
 // JESD204 PCS with Link Training - Source File List
-// All paths relative to the 'code' directory
+// Paths relative to the project root directory
 
 // Top-level wrapper with link training
-jesd204_pcs_link_training.v
+code/jesd204_pcs_link_training.v
 
 // Original soft PCS wrapper (for reference)
-jesd204_soft_pcs_wrapper.v
-jesd204_soft_pcs_fifo.v
+code/jesd204_soft_pcs_wrapper.v
+code/jesd204_soft_pcs_fifo.v
 
 // TX modules
-jesd204_soft_pcs_tx.v
-jesd204_8b10b_encoder.v
-jesd204_tx_ctrl.v
-jesd204_tx_lane.v
+code/jesd204_soft_pcs_tx.v
+code/jesd204_8b10b_encoder.v
+code/jesd204_tx_ctrl.v
+code/jesd204_tx_lane.v
 
 // RX modules
-jesd204_soft_pcs_rx.v
-jesd204_8b10b_decoder.v
-jesd204_pattern_align.v
-jesd204_rx_cgs.v
-jesd204_ilas_monitor.v
-jesd204_rx_ctrl.v
-jesd204_rx_lane.v
-jesd204_rx_frame_align.v
-align_mux.v
-elastic_buffer.v
+code/jesd204_soft_pcs_rx.v
+code/jesd204_8b10b_decoder.v
+code/jesd204_pattern_align.v
+code/jesd204_rx_cgs.v
+code/jesd204_ilas_monitor.v
+code/jesd204_rx_ctrl.v
+code/jesd204_rx_lane.v
+code/jesd204_rx_frame_align.v
+code/align_mux.v
+code/elastic_buffer.v
 
 // Common modules
-jesd204_scrambler.v
-jesd204_lmfc.v
-jesd204_frame_mark.v
-jesd204_frame_align_replace.v
+code/jesd204_scrambler.v
+code/jesd204_lmfc.v
+code/jesd204_frame_mark.v
+code/jesd204_frame_align_replace.v
 
 // Utility modules
-util_pipeline_stage.v
-sync_bits.v
-sync_event.v
+code/util_pipeline_stage.v
+code/sync_bits.v
+code/sync_event.v
